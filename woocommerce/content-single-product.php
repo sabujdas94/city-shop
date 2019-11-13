4e67aee24bd533403861d19d0e8b16411b139b8b
Sabuj Das <sabujdas94@gmail.com>
2019-11-14 04:04:35 +0600

    Make feature and specification dynamic

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -109,46 +109,29 @@ if (post_password_required()) {
                 <div class="card">
                     <div class="card-header" id="headingOne">
                         <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne"
                                 aria-expanded="true" aria-controls="collapseOne">
                                 FEATURES
                             </button>
                         </h2>
                     </div>
-                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne"
                         data-parent="#productAccordion">
-                        <div class="card-body">
-                            <h6>PRODUCT DIMENTIONS:</h6>
-                            Chopistics Size: 9"/23cm <br>
-                            Aluminium Case Size: 10"/25.4cm
-                            <br><br>
-                            <h6>PRODUCT SIZE:</h6>
-                            Width: 1.2"/3cm <br>
-                            Length: 10"/25.4cm
-                            <br><br>
-                            <h6>PRODUCT WEIGHT:</h6>
-                            45gm/1.6 ounce Includes Aluminium Case Weigth
-                        </div>
+                        <div class="card-body"><?php echo nl2br(get_field('features')); ?></div>
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-header" id="headingTwo">
                         <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                            <button class="btn btn-link" type="button" data-toggle="collapse"
                                 data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                                 SPECIFICATIONS
                             </button>
                         </h2>
                     </div>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#productAccordion">
+                    <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#productAccordion">
                         <div class="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
-                            squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa
-                            nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
-                            single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft
-                            beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
-                            lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
-                            probably haven't heard of them accusamus labore sustainable VHS.
+                            <?php echo nl2br(get_field('specification')); ?>
                         </div>
                     </div>
                 </div>
